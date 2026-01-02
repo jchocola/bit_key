a3de1ae9a88fc697a85ffadcdb65c5a0aa53d060
Bak Nguen <sangsangden@gmail.com>
2026-01-02 17:04:50 +0700

    account security page

---
+++ lib/features/feature_setting/presentation/pages/acc_security_page/acc_security_page.dart
@@ -0,0 +1,61 @@
+import 'package:bit_key/core/constants/app_constant.dart';
+import 'package:bit_key/core/theme/app_bg.dart';
+import 'package:bit_key/features/feature_setting/presentation/pages/acc_security_page/widgets/acc_sec_appbar.dart';
+import 'package:bit_key/shared/widgets/custom_listile.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class AccSecurityPage extends StatelessWidget {
+  const AccSecurityPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AppBg(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: AppConstant.appPadding, vertical: AppConstant.appPadding),
+          child: Column(
+            spacing: AppConstant.appPadding,
+            children: [
+              AppSecPageAppbar(),
+              SizedBox(height: AppConstant.appPadding),
+              CustomListile(
+                icon: Icons.fingerprint,
+                title: 'Face ID',
+                trailingValue: 'Enabled',
+              ),
+
+              CustomListile(
+                icon: Icons.fingerprint,
+                title: 'Pin Code',
+                trailingValue: 'Enabled',
+              ),
+
+              CustomListile(
+                icon: Icons.fingerprint,
+                title: 'Session Timeout',
+                trailingValue: 'Enabled',
+              ),
+
+               CustomListile(
+                icon: Icons.fingerprint,
+                title: 'Change Master Password',
+                trailingValue: 'Enabled',
+              ),
+
+
+                CustomListile(
+                icon: Icons.fingerprint,
+                title: 'Чистка вреенных ключей',
+                trailingValue: 'каждый час',
+              ), 
+
+              Text('Внимание , после очистки временных ключей, потребуется повторный вход во аккаунты. Face ID и Pin Code работать не будут до повторного ввода основных учетных данных.'), 
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/feature_setting/presentation/setting_page.dart
+++ lib/features/feature_setting/presentation/setting_page.dart
@@ -1,5 +1,6 @@
 import 'package:bit_key/core/constants/app_constant.dart';
 import 'package:bit_key/core/icon/app_icon.dart';
+import 'package:bit_key/features/feature_setting/presentation/pages/acc_security_page/acc_security_page.dart';
 import 'package:bit_key/features/feature_setting/presentation/widgets/setting_appbar.dart';
 import 'package:bit_key/shared/widgets/custom_listile.dart';
 import 'package:flutter/material.dart';
@@ -15,13 +16,27 @@ class SettingPage extends StatelessWidget {
         spacing: AppConstant.appPadding,
         children: [
           SettingPageAppbar(),
-        
-          CustomListile(title: 'Account security', icon: AppIcon.securityIcon,),
-           CustomListile(title: 'Language', icon: AppIcon.languageIcon,),
-           CustomListile(title: 'Vault', icon: AppIcon.vaultIcon,),
-            CustomListile(title: 'About', icon: AppIcon.infoIcon,),
-            //  CustomListile(),
-            //   CustomListile(),
+
+          CustomListile(
+            title: 'Account security',
+            icon: AppIcon.securityIcon,
+            onTap: () {
+              showModalBottomSheet(
+                context: context,
+                isScrollControlled: true,
+                builder: (context) {
+                  return SizedBox(
+                      height: MediaQuery.of(context).size.height * AppConstant.modalPageHeight,
+                    child: const AccSecurityPage());
+                },
+              );
+            },
+          ),
+          CustomListile(title: 'Language', icon: AppIcon.languageIcon),
+          CustomListile(title: 'Vault', icon: AppIcon.vaultIcon),
+          CustomListile(title: 'About', icon: AppIcon.infoIcon),
+          //  CustomListile(),
+          //   CustomListile(),
         ],
       ),
     );
