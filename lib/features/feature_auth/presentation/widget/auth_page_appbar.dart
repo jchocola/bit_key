ff2e6bacbdaec06ec052d4406218713d56c4f1a2
Bak Nguen <sangsangden@gmail.com>
2026-01-12 20:14:48 +0700

    auth page appbar added

--- lib/features/feature_auth/presentation/widget/auth_page_appbar.dart
+++ lib/features/feature_auth/presentation/widget/auth_page_appbar.dart
@@ -2,9 +2,13 @@ import 'package:bit_key/core/app_text/app_text.dart';
 import 'package:bit_key/core/constants/app_constant.dart';
 import 'package:bit_key/core/icon/app_icon.dart';
 import 'package:bit_key/core/theme/app_color.dart';
+import 'package:bit_key/features/feature_setting/presentation/pages/about_page/about_page.dart';
+import 'package:bit_key/features/feature_setting/presentation/pages/language_page/bloc/language_bloc.dart';
+import 'package:bit_key/main.dart';
 import 'package:bit_key/shared/widgets/search_textfiled.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:glassy_real_navbar/glassy_real_navbar.dart';
 import 'package:liquid_glass_renderer/liquid_glass_renderer.dart';
 
@@ -24,12 +28,58 @@ class AuthPageAppbar extends StatelessWidget {
 
         child: Container(
           padding: EdgeInsets.all(AppConstant.appPadding),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            spacing: AppConstant.appPadding,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text(context.tr(AppText.verifyMasterKey)),
-           
+              Spacer(),
+              Row(
+                spacing: AppConstant.appPadding,
+                children: [
+                  PopupMenuButton(
+                    child: Icon(AppIcon.languageIcon),
+                    itemBuilder: (context) {
+                      return List.generate(context.supportedLocales.length, (
+                        index,
+                      ) {
+                        final Locale locale = context.supportedLocales[index];
+                        return PopupMenuItem(
+                          child: Text(locale.languageCode),
+                          onTap: () {
+                            context.setLocale(locale);
+
+                            logger.e(context.locale.toString());
+
+                            context.read<LanguageBloc>().add(
+                              LanguageBlocEvent_setLangCode(
+                                langCode: locale.languageCode,
+                              ),
+                            );
+                          },
+                        );
+                      });
+                    },
+                  ),
+
+                  IconButton(
+                    onPressed: () {
+                      showModalBottomSheet(
+                        isScrollControlled: true,
+                        context: context,
+                        builder: (context) {
+                          return SizedBox(
+                            height:
+                                MediaQuery.of(context).size.height *
+                                AppConstant.modalPageHeight,
+                            child: AboutPage(),
+                          );
+                        },
+                      );
+                    },
+                    icon: Icon(AppIcon.infoIcon),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
